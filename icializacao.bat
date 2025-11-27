d178f3acfc22d760157641c20229225b8110c11d
Enrique Silveira <105016903+enrique-ss@users.noreply.github.com>
2025-11-27 14:02:57 -0300

    arrumado .bat
    antes o edge abria sempre em tela cheia, não apenas pro wallpaper

--- icializacao.bat
+++ icializacao.bat
@@ -1,2 +1,5 @@
 @echo off
-start "" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --guest --start-fullscreen "file:///C:/Users/LUIZHENRIQUESILVEIRA/Desktop/Pessoal/fake-wallpaper-mp4/index.html" --no-first-run
+REM Use o argumento --user-data-dir para criar um perfil dedicado,
+REM isolando a configuração de tela cheia (fullscreen) do seu Edge normal.
+
+start "" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --user-data-dir="C:\EdgeWallpaperProfile" --guest --start-fullscreen "file:///C:/Users/LUIZHENRIQUESILVEIRA/Desktop/Pessoal/fake-wallpaper-mp4/index.html" --no-first-run
